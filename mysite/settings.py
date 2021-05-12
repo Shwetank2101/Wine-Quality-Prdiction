e46665d87ab178463512a43a864d3a6b9171fc46
Shwetank2101 <shwetankdixitofficial@gmail.com>
2021-05-13 03:30:49 +0530

    update setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
