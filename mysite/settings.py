dd9c32d0cc7e4384b735d1323fdc3c693191d167
Shwetank2101 <shwetankdixitofficial@gmail.com>
2021-05-12 18:32:08 +0530

    'Heroku config

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,4 +123,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #new
